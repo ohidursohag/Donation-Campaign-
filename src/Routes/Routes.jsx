1cc13af5bce7bc3508ab6ed7f466d0298f83326f
ohidursohag <ohidursohag9595@gmail.com>
2023-09-24 19:33:52 +0600

    React App Environment Setup & First Commit

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,10 @@
+import { createBrowserRouter } from "react-router-dom";
+
+const myCreatedRoutes = createBrowserRouter([
+   {
+      path: '/',
+      element: <div>Helo Donation</div>
+   }
+])
+
+export default myCreatedRoutes
