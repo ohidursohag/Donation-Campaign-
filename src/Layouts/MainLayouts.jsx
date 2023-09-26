35ec580284dd1bf04474f36be694141fdd9973ef
ohidursohag <ohidursohag9595@gmail.com>
2023-09-26 21:04:39 +0600

    added back button + error lottie animation in error page  and loading page animation using lottie animation

--- src/Layouts/MainLayouts.jsx
+++ src/Layouts/MainLayouts.jsx
@@ -1,11 +1,16 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 import NavBar from "../Components/NavBarComponents/NavBar/NavBar";
-
+import LoadingAnimation from "../Components/LoadingAnimation/LoadingAnimation";
 const MainLayouts = () => {
+   const navigation = useNavigation()
    return (
       <div className="font-[Inter,sans-serif] ">
          <NavBar></NavBar>
-         <Outlet></Outlet>
+         <div className="flex justify-center items-center -z-20">
+            {
+               navigation.state === 'loading' ? <LoadingAnimation /> : <Outlet></Outlet>
+            }
+         </div>
       </div>
    );
 };
